77378d433442959b75780885ca3173f21eea05bc
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-07-31 18:07:14 +0200

    test_time_transformer_parametrized

--- tests/test_transformers_parametrized.py
+++ tests/test_transformers_parametrized.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
-from src.transformers import GoalAdjustor
+from src.transformers import GoalAdjustor, TimeTransformer
 
 test_goal_transformer_testdata = [
     (pd.DataFrame({'goal': [5], 'static_usd_rate': [2]}),
@@ -27,5 +27,23 @@ def test_goal_adjustor(sample_df, expected_df):
     adjustor = GoalAdjustor()
 
     result_df = adjustor.transform(sample_df)
+
+    assert_frame_equal(result_df, expected_df)
+
+
+test_time_transformer_testdata = [
+    (pd.DataFrame({'deadline': [1459283229], 'created_at': [1455845363],
+     'launched_at': [1456694829]}),
+     pd.DataFrame({'launched_to_deadline': [29], 'created_to_launched': [9]})),
+    (pd.DataFrame({'deadline': [0], 'created_at': [0], 'launched_at': [0]}),
+     pd.DataFrame({'launched_to_deadline': [0], 'created_to_launched': [0]}))
+]
+
+@pytest.mark.parametrize(
+    "sample_df, expected_df", test_time_transformer_testdata)
+def test_time_transformer(sample_df, expected_df):
+    time_transformer = TimeTransformer()
+
+    result_df = time_transformer.transform(sample_df)
 
     assert_frame_equal(result_df, expected_df)
